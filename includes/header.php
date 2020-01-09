7b1b8b05542b243930c44e0a8e18b37eb5a67104
medicare <mcareducation@gmail.com>
2020-01-09 15:52:56 +0700

    conv to php

---
+++ includes/header.php
@@ -0,0 +1,14 @@
+<header class="header-section clearfix">
+		<a href="index.php" class="site-logo">
+			<h2 class="hs-text">
+				<img src="images/logo.png" height="40" alt="" srcset="">
+			</h2>
+
+		</a>
+		<div class="header-right">
+			<a href="#" class="hr-btn">Help</a>
+			<span>|</span>
+		</div>
+		<div class="main-menu">
+		</div>
+	</header>
---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+	<title>Check penjurusan</title>
+	<meta charset="UTF-8">
+	<meta name="description" content="SolMusic HTML Template">
+	<meta name="keywords" content="music, html">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<!-- Google font -->
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i&display=swap" rel="stylesheet">
+
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="css/bootstrap.min.css" />
+	<link rel="stylesheet" href="css/font-awesome.min.css" />
+	<link rel="stylesheet" href="css/owl.carousel.min.css" />
+	<link rel="stylesheet" href="css/slicknav.min.css" />
+
+	<!-- Main Stylesheets -->
+	<link rel="stylesheet" href="css/style.css" />
+</head>
+
+<body>
+	<!-- Page Preloder -->
+
+	<div id="preloder">
+		<div class="loader"></div>
+	</div>
+	<!-- Header section -->
+	<?php
+		include 'includes/header.php'
+	?>
+	<!-- Header section end -->
+	<section class="hero-section">
+		<div class="hero-slider owl-carousel">
+			<div class="hs-item">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-6">
+							<div class="hs-text">
+								<h2><span>Pengumpulan</span> Data </h2>
+								<p> Merupakan Proses Pengumpulan data Ujian Nasional Mahasiswa <a href="#" class="text-primary">Univerisitas Bumigora </a> untuk perancangan aplikasi sistem rekomendasi pemilihan program study di <a href="#" class="text-primary">Univerisitas Bumigora </a> </p>
+								<p>PILIH SALAH SATU LINK DI BAWAH SESUAI JURUSAN DI SMA (IPA/IPS) atau SMK</p>
+								<a href="#" class="site-btn" target="_blank">choose for SMA <span class="text-warning">
+										<h3>IPA</h3>
+									</span></a>
+								<a href="#" class="site-btn" target="_blank">choose for SMA <span class="text-warning">
+										<h3>IPS</h3>
+									</span></a>
+								<a href="#" class="site-btn" target="_blank">choose for SMK <span class="text-warning">
+										<h3>TKJ</h3>
+									</span></a>
+							</div>
+						</div>
+						<div class="col-lg-6">
+							<div class="hr-img">
+								<img src="images/result1.png" alt="">
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+
+	<?php
+		include 'includes/footer.php'
+	?>
+	 <!-- Footer section end -->
+
+	<!--====== Javascripts & Jquery ======-->
+	<script src="js/jquery-3.2.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.slicknav.min.js"></script>
+	<script src="js/owl.carousel.min.js"></script>
+	<script src="js/mixitup.min.js"></script>
+	<script src="js/main.js"></script>
+
+</body>
+
+</html>
